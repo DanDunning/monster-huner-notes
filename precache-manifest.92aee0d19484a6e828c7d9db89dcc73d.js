5aed14141e27a11ce4762a8c907f859f81e17ed1
Dan Dunning <danieldunning@Macbook-2.local>
2019-09-29 16:53:54 -0400

    Updates

---
+++ precache-manifest.92aee0d19484a6e828c7d9db89dcc73d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2b69f695a767d551763e319621b62af",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f15191e1f0de29270fcd",
+    "url": "/static/css/main.cea2070a.chunk.css"
+  },
+  {
+    "revision": "cf0b22ea1f04a1c4bd25",
+    "url": "/static/js/2.e27a2a37.chunk.js"
+  },
+  {
+    "revision": "f15191e1f0de29270fcd",
+    "url": "/static/js/main.0649d407.chunk.js"
+  },
+  {
+    "revision": "38baab03656df6e9ce10",
+    "url": "/static/js/runtime-main.a121462c.js"
+  }
+]);
